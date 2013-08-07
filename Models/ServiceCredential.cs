2569c9b28642a04d4fa85fb6e6ba2fcf2d3b752d
Dan Higham <dan.higham@gmail.com>
2013-08-07 14:16:31 +0100

    Remove credential comment!

--- Models/ServiceCredential.cs
+++ Models/ServiceCredential.cs
@@ -1,5 +1,3 @@
-//	VCAP_SERVICES={"cleardb-n/a":[{"name":"cleardb-4fddc","label":"cleardb-n/a","tags":[],"plan":"spark","credentials":{"jdbcUrl":"jdbc:mysql://b5ae74fdb3e19d:d38ec28e@us-cdbr-east-04.cleardb.com:3306/ad_a6f52a023ac7f7f","uri":"mysql://b5ae74fdb3e19d:d38ec28e@us-cdbr-east-04.cleardb.com:3306/ad_a6f52a023ac7f7f?reconnect=true","name":"ad_a6f52a023ac7f7f","hostname":"us-cdbr-east-04.cleardb.com","port":"3306","username":"b5ae74fdb3e19d","password":"d38ec28e"}}]}
-
 using System;
 using System.Runtime.Serialization;
 

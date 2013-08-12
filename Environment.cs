4b5696bc92dd7686566f983b4a1d0e10f6eda653
Dan Higham <dan.higham@gmail.com>
2013-08-12 12:59:42 +0100

    Added ConfigurationManager settings

--- Environment.cs
+++ Environment.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Configuration;
 using System.IO;
+using System.Linq;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -13,7 +15,7 @@ namespace CloudFoundry.Mono
 		public static List<Service> ProvisionedServices ()
 		{
 			var servicesJSON = System.Environment.GetEnvironmentVariable ("VCAP_SERVICES");
-			List<Service> _services = new List<Service> ();
+			List<Service> services = new List<Service> ();
 
 			JsonTextReader jtr = new JsonTextReader (new StringReader(servicesJSON));
 			JsonSerializer se = new JsonSerializer();
@@ -22,18 +24,41 @@ namespace CloudFoundry.Mono
 			var _topLevel = parsedData.First;
 
 			foreach (var serviceProvider in _topLevel) {
-				foreach (var serviceJSON in serviceProvider)
-					_services.Add(JsonConvert.DeserializeObject<Service> 
-						(serviceJSON.ToString ()));
+				foreach (var serviceJSON in serviceProvider) {
+					Service service = JsonConvert.DeserializeObject<Service> (serviceJSON.ToString ());
+					services.Add (service);
+					AddServiceToDotNETConfiguration (service);
+				}
 			};
 
-			return _services;
+			return services;
 		}
 
 		public static Application Application()
 		{
 			var applicationJSON = System.Environment.GetEnvironmentVariable ("VCAP_APPLICATION");
 			return JsonConvert.DeserializeObject<Application> (applicationJSON);
+		}
+
+		private static void AddServiceToDotNETConfiguration(Service service) 
+		{
+			ConnectionStringSettingsCollection settings =
+				ConfigurationManager.ConnectionStrings;
+
+			if (settings[service.Name] == null) {
+//			if (((IEnumerable<ConnectionStringSettings>)settings.GetEnumerator()).Where (s => s.Name == service.Name).First() == null) {
+				ConnectionStringSettings setting = new ConnectionStringSettings ();
+				setting.ConnectionString = "server=" + service.Credentials.Hostname + 
+					";database=" + service.Credentials.DatabaseName + 
+					";uid=" + service.Credentials.Username + 
+					";password=" + service.Credentials.Password +
+					";port=" + service.Credentials.Port;
+
+//				setting.ProviderName = 
+				setting.Name = service.Name;
+
+				settings.Add (setting);
+			}
 		}
 	}
 }
